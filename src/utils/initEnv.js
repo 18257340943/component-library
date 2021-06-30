ff68c0878f4c72a884344de69211feda0c77478a
rzj1440277049 <18257340943@163.com>
2021-06-30 16:30:53 +0800

    feat: 4.30

--- src/utils/initEnv.js
+++ src/utils/initEnv.js
@@ -2,9 +2,9 @@
 console.log(buildEnv, appName, 'buildEnv, appName');
 class InitEnv {
   constructor() {
-    this.notLoginUrl = `http://${buildEnv === "online" ? "" : "pre-new-saas."}zxhj618.com/login`;
-    this.baseUrl = `http://${buildEnv === "online" ? "" : "pre-"}main-service.zxhj618.com`;
-    this.mallUrl = `http://${buildEnv === "online" ? "" : "pre-"}saas-mall.zxhj618.com`;
+    this.notLoginUrl = `//${buildEnv === "online" ? "" : "pre-new-saas."}zxhj618.com/login`;
+    this.baseUrl = `//${buildEnv === "online" ? "" : "pre-"}main-service.zxhj618.com`;
+    this.mallUrl = `//${buildEnv === "online" ? "" : "pre-"}saas-mall.zxhj618.com`;
     this.cookieName = this.matchCookieName();
     this.domain = process.env.NODE_ENV === "production" ? "domain=zxhj618.com;" : "";
   }
